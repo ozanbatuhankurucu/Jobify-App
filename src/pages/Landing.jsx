024bcce2c1ebead6c57c6e3b78a79f5c1c40a1f3
ozanbatuhankurucu <ozanbatusss@gmail.com>
2023-09-04 19:18:57 +0300

    Add styled components

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,3 +1,15 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: aquamarine;
+  padding: 10px 20px;
+`
+
 export const Landing = () => {
-  return <div>Landing</div>
+  return (
+    <div>
+      Landing
+      <Button>Test button</Button>
+    </div>
+  )
 }
